4ca5169d0898c6c3e589068b63028277a9da3415
Yucheng-An <yuc.an2019@gmail.com>
2025-02-14 03:03:10 -0800

    Modify download-data.py download part from True to False

--- download-data.py
+++ download-data.py
@@ -6,8 +6,8 @@ import torchvision.transforms as transforms
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 #Download training data and test data
-trainset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-testset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+trainset = torchvision.datasets.MNIST(root="./data", train=True, download=False, transform=transform)
+testset = torchvision.datasets.MNIST(root="./data", train=False, download=False, transform=transform)
 
 #Create a data loader
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
