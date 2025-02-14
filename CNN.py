827e001a6a518d88072ec10739d3254a477a28bc
Yucheng-An <yuc.an2019@gmail.com>
2025-02-14 03:32:21 -0800

    Save the trained model

--- CNN.py
+++ CNN.py
@@ -1,20 +1,22 @@
-import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
-import numpy as np
-
-
 
 # Define data preprocessing
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
 #Download training data and test data
 trainset = torchvision.datasets.MNIST(root="./data", train=True, download=False, transform=transform)
+print(trainset)
 testset = torchvision.datasets.MNIST(root="./data", train=False, download=False, transform=transform)
+"""
+Auto Download the Dataset
+trainset = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
+testset = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+"""
 
 #Create a data loader
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
@@ -26,7 +28,7 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2) 
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc1 = nn.Linear(64 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 10)
 
@@ -39,17 +41,20 @@ class CNN(nn.Module):
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-print("Module setup completed!")
+
+
+print(f"Module setup completed!")
 model = CNN()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
+print(f"The device has been choose {device}!")
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-num_epochs = 5
 
+num_epochs = 8
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
@@ -61,7 +66,8 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss / len(trainloader):.4f}")
+    print(f"Epoch {epoch} finished!")
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(trainloader):.4f}")
 
 print("Training Completed!")
 
@@ -79,7 +85,6 @@ with torch.no_grad():
 
 print(f"Testset Accuracy: {100 * correct / total:.2f}%")
 
-
 images, labels = next(iter(testloader))
 model.eval()
 outputs = model(images.to(device))
@@ -94,4 +99,6 @@ for i in range(10):
     axes[i].axis("off")
 
 plt.show()
-
+# Save the trained model
+torch.save(model.state_dict(), "mnist_cnn.pth")
+print("Model saved successfully as mnist_cnn.pth!")
