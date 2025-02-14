5936ac91c94888240c7337200da6146b810b2531
Yucheng-An <yuc.an2019@gmail.com>
2025-02-14 03:09:43 -0800

    Calculate the accuracy

--- download-data.py
+++ download-data.py
@@ -65,4 +65,18 @@ for epoch in range(num_epochs):
 
 print("Training Completed!")
 
+model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in testloader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f"Testset Accuracy: {100 * correct / total:.2f}%")
+
 
